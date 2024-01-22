5c24915748bb31ff113ddc885b3b16784984a4d5
damarizz <138645520+damarizz@users.noreply.github.com>
2024-01-21 21:43:10 -0500

    interactuando con html

---
+++ 2035-logica-programacion-2-Aula1/app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1'); // permite acceder a c/u de los elementos
+let parrafo = document.querySelector("p");
+
+titulo.innerHTML = "Juego del Número Secreto";
+parrafo.innerHTML = "Indica un número del 1 al 10.";
+
+function intentoDeUsuario() {
+    alert("Click desde el botón");
+}
